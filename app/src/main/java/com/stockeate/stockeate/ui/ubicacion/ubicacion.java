cb970df25deee4b8f1242935a96171d394ad860b
Vanesa Barrale <vbarrale@ams.red>
2021-04-28 14:27:57 -0300

    google maps

--- app/src/main/java/com/stockeate/stockeate/ui/ubicacion/ubicacion.java
+++ app/src/main/java/com/stockeate/stockeate/ui/ubicacion/ubicacion.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.stockeate.stockeate.R;
@@ -43,41 +44,10 @@ public class ubicacion extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         Log.d("Aca", "Por aca paso");
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
 
-        Log.d("Aca", "Por aca paso post if");
-        mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled (true);
-
-        MarkerOptions markerOptions = new MarkerOptions();
         LatLng san_francisco = new LatLng(-31.4249815, -62.0840299);
-        markerOptions.position(san_francisco);
-        markerOptions.title(san_francisco.latitude + " : " + san_francisco.longitude);
-        mMap.clear();
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(san_francisco));
-        mMap.addMarker(markerOptions);
-
-        Log.d("Aca", "Por aca paso post mMap");
-
-        // Add a marker in San Francisco and move the camera
-        /*LatLng san_francisco = new LatLng(-31.4249815, -62.0840299);
         mMap.addMarker(new MarkerOptions().position(san_francisco).title("San Francisco"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(san_francisco));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(san_francisco, 30));*/
-
-        LatLng hiper = new LatLng(-31.42773493119209, -62.11414910012128);
-        mMap.addMarker(new MarkerOptions().position(hiper).title("Hipermercado Anselmi"));
-        LatLng pinguino = new LatLng(-31.6629403, -63.0473148);
-        mMap.addMarker(new MarkerOptions().position(pinguino).title("Pingüino"));
-        LatLng chapulin_castelli = new LatLng(-31.4222692, -62.0885534);
-        mMap.addMarker(new MarkerOptions().position(chapulin_castelli).title("Chapulin"));
-        LatLng chapulin_9_julio = new LatLng(-31.4266457, -63.2006849);
-        mMap.addMarker(new MarkerOptions().position(chapulin_9_julio).title("Chapulin"));
-        LatLng vea = new LatLng(-31.428872563164568, -62.08971817117201);
-        mMap.addMarker(new MarkerOptions().position(vea).title("Vea"));
-        LatLng norte = new LatLng(-31.417809772868658, -62.08937372955097);
-        mMap.addMarker(new MarkerOptions().position(norte).title("Norte"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(san_francisco));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(san_francisco, 15));
     }
 }
