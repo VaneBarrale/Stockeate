07013497a3d7efb4b9bb3d766dacf186dad37b00
Vanesa Barrale <vbarrale@ams.red>
2021-06-11 20:45:47 -0300

    Marcar destino

--- app/src/main/java/com/stockeate/stockeate/ui/ubicacion/ubicacion.java
+++ app/src/main/java/com/stockeate/stockeate/ui/ubicacion/ubicacion.java
@@ -6,8 +6,8 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.LocaleList;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,11 +23,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.stockeate.stockeate.R;
+import com.stockeate.stockeate.utiles.ParserTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import static com.stockeate.stockeate.R.layout.fragment_ubicacion;
 
 public class ubicacion extends Fragment implements OnMapReadyCallback {
@@ -63,15 +71,12 @@ public class ubicacion extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
         Log.d("Aca", "Por aca paso");
         miUbicacion();
-
         /*LatLng san_francisco = new LatLng(-31.4249815, -62.0840299);
         mMap.addMarker(new MarkerOptions().position(san_francisco).title("San Francisco"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(san_francisco));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(san_francisco, 15));*/
 
     }
-
-    //------> REVISAR PORQUE ENTRA AL IF, NO TOMA PERMISO DE ADMISIÓN DE GEOLOCALIZACION
 
     private void miUbicacion() {
         Log.d("Mi ubicacion ", "Entro");
@@ -100,6 +105,8 @@ public class ubicacion extends Fragment implements OnMapReadyCallback {
         Log.d("Mi ubicacion actual ", "Entro");
 
         LatLng coordenadas = new LatLng(lat, lon);
+        LatLng destino = new LatLng(-31.4249992, -62.0841599);
+        comoLlegar(coordenadas, destino);
         CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
         if (marcador != null) {
             marcador.remove();
@@ -131,4 +138,93 @@ public class ubicacion extends Fragment implements OnMapReadyCallback {
         public void onStatusChanged(String provider, int status, Bundle extras) {
         }
     };
+
+    private void comoLlegar(LatLng origen, LatLng destino){
+        Log.d("Como llegar ", "Entro");
+
+        MarkerOptions marcadorDestino= new MarkerOptions();
+        marcadorDestino.position(destino);
+        marcadorDestino.title("Este es tu destino");
+
+        String url = obtenerDireccionesURL(origen, destino);
+        DownloadTask downloadTask = new DownloadTask();
+        downloadTask.execute(url);
+    }
+
+    private String obtenerDireccionesURL(LatLng origin,LatLng dest){
+        Log.d("Obtener direcciones ", "Entro");
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+        String sensor = "sensor=false";
+        String parameters = str_origin+"&"+str_dest+"&"+sensor;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+        return url;
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            Log.d("DowloadTask ", "Entro");
+
+            String data = "";
+
+            try{
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("ERROR WS",e.toString());
+            }
+            return data;
+        }
+
+        private String downloadUrl(String strUrl) throws IOException {
+            String data = "";
+            InputStream iStream = null;
+            HttpURLConnection urlConnection = null;
+            try{
+                URL url = new URL(strUrl);
+
+                // Creamos una conexion http
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                // Conectamos
+                urlConnection.connect();
+
+                // Leemos desde URL
+                iStream = urlConnection.getInputStream();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+                StringBuffer sb = new StringBuffer();
+
+                String line = "";
+                while( ( line = br.readLine()) != null){
+                    sb.append(line);
+                }
+
+                data = sb.toString();
+
+                br.close();
+
+            }catch(Exception e){
+                Log.d("Exception", e.toString());
+            }finally{
+                iStream.close();
+                urlConnection.disconnect();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            parserTask.execute(result);
+        }
+    }
+
 }
