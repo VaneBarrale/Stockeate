ce72770593db0208413383f98b81bd34053e665b
Vanesa Barrale <vbarrale@ams.red>
2021-08-04 10:11:27 -0300

    Alta locales con validaciones

--- app/src/main/java/com/stockeate/stockeate/ui/nuevo_comercio/nuevo_comercio.java
+++ app/src/main/java/com/stockeate/stockeate/ui/nuevo_comercio/nuevo_comercio.java
@@ -32,7 +32,7 @@ import java.io.IOException;
 public class nuevo_comercio extends Fragment {
 
     private NuevoComercioViewModel viewModelNuevoComercio;
-    private Button btn_volver, btn_agregar;
+    private Button btn_volver, btn_validar, btn_agregar;
     private EditText txt_cuit, txt_Descripcion, txt_Domicilio;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,12 +44,33 @@ public class nuevo_comercio extends Fragment {
             }
         });
         btn_volver = root.findViewById(R.id.btn_Volver);
+        btn_validar = root.findViewById(R.id.btn_validar);
         btn_agregar = root.findViewById(R.id.btn_agregar);
         txt_cuit = root.findViewById(R.id.etxtCuit);
         txt_Descripcion = root.findViewById(R.id.etxtDescripcion);
         txt_Domicilio = root.findViewById(R.id.etxtDomicilio);
 
         btn_agregar.setEnabled(false);
+
+        btn_validar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!txt_cuit.getText().toString().isEmpty()){
+                    try {
+                        listarlocales(txt_cuit.getText().toString());
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    Toast.makeText(getContext(), "Ingrese nro CUIT", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         btn_volver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,50 +83,40 @@ public class nuevo_comercio extends Fragment {
             }
         });
 
-        String cuit = txt_cuit.getText().toString();
-
-        try {
-            listarlocales(cuit);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
         return root;
-    }
-
-    private void limpiarDatos() {
-        txt_cuit.setText("");
-        txt_Descripcion.setText("");
-        txt_Domicilio.setText("");
     }
 
     private void listarlocales(String cuit) throws IOException, JSONException {
 
         String jsonFileContent = utiles.leerJson(getContext(), "locales.json");
         JSONArray jsonArray = new JSONArray(jsonFileContent);
-
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObj = jsonArray.getJSONObject(i);
-            if (!jsonObj.getString("cuit").equals(cuit)){
-                Log.d("Adentro del primer if", "cuit " + cuit);
+            if (!jsonObj.getString("cuit").equals(cuit)) {
                 btn_agregar.setEnabled(true);
-                if((!txt_Descripcion.getText().toString().isEmpty()) && (!txt_Domicilio.getText().toString().isEmpty())){
-                    btn_agregar.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
+                btn_agregar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("Adentro del primer if", "cuit " + cuit);
+                        if ((!txt_Descripcion.getText().toString().isEmpty()) && (!txt_Domicilio.getText().toString().isEmpty())) {
                             Toast.makeText(getContext(), "Guardado con exito", Toast.LENGTH_SHORT).show();
                             limpiarDatos();
+                        } else {
+                            Toast.makeText(getContext(), "Complete todos los datos", Toast.LENGTH_SHORT).show();
                         }
-                    });
-                }
-                else{
-                    Toast.makeText(getContext(), "Complete todos los datos", Toast.LENGTH_SHORT).show();
-                }
-            } else {
+                    }
+                });
+            }else {
+                btn_agregar.setEnabled(false);
                 Toast.makeText(getContext(), "Ya existe un local con ese CUIT", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void limpiarDatos() {
+        txt_cuit.setText("");
+        txt_Descripcion.setText("");
+        txt_Domicilio.setText("");
+        btn_agregar.setEnabled(false);
     }
 }
