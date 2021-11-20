e277e28884b374a8ef539060a5a3a7e716c6af80
Vanesa Barrale <vbarrale@ams.red>
2021-11-20 16:18:08 -0300

    Consumir Apis Locales, Login, Register

--- app/src/main/java/com/stockeate/stockeate/clases/class_locales.java
+++ app/src/main/java/com/stockeate/stockeate/clases/class_locales.java
@@ -1,92 +1,37 @@
 package com.stockeate.stockeate.clases;
 
+import kotlin.collections.ULongIterator;
+
 public class class_locales {
-    private String id, nombre_fantasia, razon_social, domicilio, departamento, piso, localidad, provincia, cuit;
-    private int codigo_postal;
+    private String name, latitud, longitud, cuit;
 
     public class_locales() {
-        this.id = id;
-        this.nombre_fantasia = nombre_fantasia;
-        this.razon_social = razon_social;
-        this.domicilio = domicilio;
-        this.departamento = departamento;
-        this.piso = piso;
-        this.localidad = localidad;
-        this.provincia = provincia;
-        this.codigo_postal = codigo_postal;
+        this.name = name;
+        this.latitud = latitud;
+        this.longitud = longitud;
         this.cuit = cuit;
     }
 
-    public String getId() {
-        return id;
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getLatitud() {
+        return latitud;
     }
 
-    public String getNombre_fantasia() {
-        return nombre_fantasia;
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
     }
 
-    public void setNombre_fantasia(String nombre_fantasia) {
-        this.nombre_fantasia = nombre_fantasia;
+    public String getLongitud() {
+        return longitud;
     }
 
-    public String getRazon_social() {
-        return razon_social;
-    }
-
-    public void setRazon_social(String razon_social) {
-        this.razon_social = razon_social;
-    }
-
-    public String getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(String domicilio) {
-        this.domicilio = domicilio;
-    }
-
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public String getPiso() {
-        return piso;
-    }
-
-    public void setPiso(String piso) {
-        this.piso = piso;
-    }
-
-    public String getLocalidad() {
-        return localidad;
-    }
-
-    public void setLocalidad(String localidad) {
-        this.localidad = localidad;
-    }
-
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
-    public int getCodigo_postal() {
-        return codigo_postal;
-    }
-
-    public void setCodigo_postal(int codigo_postal) {
-        this.codigo_postal = codigo_postal;
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
     }
 
     public String getCuit() {
--- app/src/main/java/com/stockeate/stockeate/ui/escanear_codigos_barra/Fragment_escanear_codigos_barra.java
+++ app/src/main/java/com/stockeate/stockeate/ui/escanear_codigos_barra/Fragment_escanear_codigos_barra.java
@@ -34,6 +34,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 
